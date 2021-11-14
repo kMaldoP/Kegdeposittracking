4c7e30de16caf611f3b334fba84b77c6e2d2a121
Krys Maldo <krysm2015@gmail.com>
2021-11-14 01:49:00 -0800

    cleaning up the code

--- deposit_database.py
+++ deposit_database.py
@@ -24,8 +24,7 @@ class User:
         self.shells = nshells
         self.taps = ntaps
         self.total = (nshells * 100) + (ntaps * 50)
-        print(
-            f"Number of shells changed to {self.shells}\nNumber of taps changed to {self.taps}\nNew total set to ${self.total}")
+        print(f"Number of shells changed to {self.shells}\nNumber of taps changed to {self.taps}\nNew total set to ${self.total}")
 
     def add_record(self):
         new_record = input("Enter Swap Record DD/MM/YYYY")
@@ -39,17 +38,17 @@ class User:
         returned_shells = int(input("Input the number of shells refunded:  "))
         self.shells -= returned_shells
         self.total = (self.shells * 100) + (self.taps * 50)
-        print(
-            f"You have changed the number of shells to {self.shells}\nYour new total is {self.total}")
+        print(f"You have changed the number of shells to {self.shells}\nYour new total is {self.total}")
 
     def tap_return(self):
-        returned_taps = int(
-            input("Enter the number of taps you are returning:  "))
+        returned_taps = int(input("Enter the number of taps you are returning:  "))
         self.taps -= returned_taps
         self.total = (self.taps * 50) + (self.shells * 100)
-        print(
-            f"you have changed the number of taps to: {self.taps}\nYour total is now: {self.total}")
+        print(f"you have changed the number of taps to: {self.taps}\nYour total is now: {self.total}")
 
+    
+            
+   
 
 user1 = User("05/08/1987", "bob", "858-205-8543", 4, 2)
 user2 = User("16/15/20", "phil", "858-234-5434", 2, 2)
